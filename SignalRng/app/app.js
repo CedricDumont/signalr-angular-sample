666d6ce68212d1c680af7dda2f8c62701aeaaea3
Absyla-Open <absyla@outlook.com>
2015-01-23 10:01:45 +0100

    implement chat message using angular and signalR

---
+++ SignalRng/app/app.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        'SignalR'
+    ]);
+
+    if (typeof String.prototype.startsWith != 'function') {
+        String.prototype.startsWith = function (str) {
+            return this.slice(0, str.length) == str;
+        };
+    }
+
+    app.value('messages', []);
+
+    app.factory('messageService', ['$rootScope', 'Hub', 'messages',
+        function messageService($rootScope, Hub, messages) {
+
+             var hub = new Hub('chatHub', {
+
+                 //client side methods
+                 listeners: {
+                     'broadcastMessage': function (author, message) {
+                         messages.push({ author: author, body: message });
+                         $rootScope.$apply();
+                     }                     
+                 },
+
+                 //server side methods
+                 methods: ['send'],
+
+                 //handle connection error
+                 errorHandler: function (error) {
+                     console.error(error);
+                 },
+               
+             });
+
+             var sendMessage = function (author, message) {
+                 hub.send(author, message); //Calling a server method
+             };
+
+             return {
+                 sendMessage: sendMessage,
+             };
+
+         }
+    ]);
+
+    app.controller('chatCtrl', ['messageService','messages',
+
+        function chatCtrl(messageService, messages) {
+
+            var vm = this;
+
+            //model
+            vm.message = '';
+            vm.name = '';
+            vm.messages = messages;
+
+            //functions
+            vm.send = function send() {
+                messageService.sendMessage(vm.name, vm.message);
+                vm.message = '';
+            };
+
+        }
+    ]);
+
+})();
